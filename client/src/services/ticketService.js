0225c94cdaebb36e2473dba6c2c8bc2c987b31e0
Ayush Yadav <yayush1034@gmail.com>
2026-09-13 12:56:00 +0530

    url updated

--- client/src/services/ticketService.js
+++ client/src/services/ticketService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL =
   import.meta.env.VITE_API_URL ||
-  "http://localhost:5000/api";
+  "https://datastraw-yel0.onrender.com/api";
 
 const apiClient = axios.create({
   baseURL: API_URL,
